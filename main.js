6658fe1c3d988748d8b6202424294ee03a02ec43
nvg1966 <nvg1966@yandex.ru>
2023-10-08 20:35:20 +0500

    day3_end

--- main.js
+++ main.js
@@ -1,25 +1,73 @@
-import 'normalize.css';
-import './style.scss';
+import "normalize.css";
+import "./style.scss";
+import Navigo from "navigo";
+import { Header } from "./modules/Header/Header";
+import { Main } from "./modules/Main/Main";
+import { Footer } from "./modules/Footer/Footer";
 
-import {Navigation, Thumbs} from 'swiper/modules';
-import Swiper  from 'swiper';
-import 'swiper/css';
+const productSlider = () => {
+  Promise.all([import("swiper/modules"), import("swiper"), import("swiper/css")]).then(
+    ([{ Navigation, Thumbs }, Swiper]) => {
+      const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
 
-const productThumbnails = new Swiper(".product__slider-thumbnails", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
+      new Swiper.default(".product__slider-main", {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".product__arrow_next",
+          prevEl: ".product__arrow_prev",
+        },
+        modules: [Navigation, Thumbs],
+        thumbs: {
+          swiper: swiperThumbnails,
+        },
+      });
+    },
+  );
+};
 
-new Swiper(".product__slider-main", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".product__arrow_next",
-    prevEl: ".product__arrow_prev",
-  },
-  modules: [Navigation, Thumbs],
-  thumbs: {
-    swiper: productThumbnails,
-  },
-});
+const Init = () => {
+
+  //const header = new Header();
+
+  new Header().mount();
+  new Main().mount();
+  new Footer().mount();
+  productSlider();
+
+  const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
+  router
+    .on("/", () => {
+      console.log("на главной");
+    })
+    .on("/category", (obj) => {
+      console.log("категория - ", obj);
+    })
+    .on("/favorite", () => {
+      console.log("избранное");
+    })
+    .on("/search", () => {
+      console.log("поиск");
+    })
+    .on("/product/:id", (obj) => {
+      console.log("продукт - ", obj);
+    })
+    .on("/cart", () => {
+      console.log("корзина");
+    })
+    .on("/order", () => {
+      console.log("заказ");
+    })
+    .notFound(() => {
+      document.body.innerHTML ='<h2>Страница не найдена</h2>';
+      console.log(404);
+    });
+
+  router.resolve();
+};
+
+Init();
