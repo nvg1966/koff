abd1f33927f27bbb8a10e65fa9311e2aeeda4c35
nvg1966 <nvg1966@yandex.ru>
2023-10-09 22:43:13 +0500

    day4_end

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import { Header } from "./modules/Header/Header";
 import { Main } from "./modules/Main/Main";
 import { Footer } from "./modules/Footer/Footer";
 import { ProductList } from "./modules/ProductList/ProductList";
+import { ApiService } from "./services/ApiService";
+import { Catalog } from "./modules/Catalog/Catalog";
 
 const productSlider = () => {
   Promise.all([import("swiper/modules"), import("swiper"), import("swiper/css")]).then(
@@ -32,55 +34,65 @@ const productSlider = () => {
 };
 
 const Init = () => {
-
-  //const header = new Header();
+  const api = new ApiService();
+  const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
 
   new Header().mount();
   new Main().mount();
   new Footer().mount();
+
+  api.getProductCategories().then((data) => {
+    new Catalog().mount(new Main().element, data);
+  });
   productSlider();
 
-  const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
   router
-    .on("/", () => {
-      new ProductList().mount(new Main().element, [1]);
-    }, {    
-      // before(done)  {
-      //   console.log("before");
-      //   done();
-      // },      
-      // after() {
-      //   console.log("after");
-      // },     
-      leave(done) {
-        console.log("leave");
-        done();
-      },     
-      already() {
-        console.log("already");
+    .on(
+      "/",
+      async () => {
+        const product = await api.getProducts();
+        new ProductList().mount(new Main().element, product);
+        router.updatePageLinks();
       },
-    })
-    .on("/category", () => {      
-      
-        new ProductList().mount(new Main().element, [1, 2, 3, 4, 5, 6], 'Категория');
+      {
+        leave(done) {
+          new ProductList().unmount();
+          done();
+        },
+        already() {
+          console.log("already");
+        },
       },
-        {
-          leave(done) {
-            console.log("leave");
-            done();
-          },
-        }
     )
-    .on("/favorite", () => {
-      
-        new ProductList().mount(new Main().element, [1, 2, 3], 'Избранное');
+    .on(
+      "/category",
+      async ({params: {slug}}) => {
+        const product = await api.getProducts();
+        new ProductList().mount(new Main().element, product, slug);
+        router.updatePageLinks();
+
       },
-        {
-          leave(done) {
-            console.log("leave");
-            done();
-          },
-        }
+      {
+        leave(done) {
+          new ProductList().unmount();
+          done();
+        },
+      },
+    )
+    .on(
+      "/favorite",
+      async () => {
+        const product = await api.getProducts();
+        new ProductList().mount(new Main().element, product, "Избранное");
+        router.updatePageLinks();
+
+      },
+      {
+        leave(done) {
+          new ProductList().unmount();
+          done();
+        },
+      },
     )
     .on("/search", () => {
       console.log("поиск");
@@ -98,11 +110,10 @@ const Init = () => {
       new Main().element.innerHTML = `
         <h2>Страница не найдена</h2>
         <p>Через 5 секунд вы будете перенаправлены на <a href="/">главную страницу</a></p>
-      `;    
+      `;
       setTimeout(() => {
-        router.navigate('/');
+        router.navigate("/");
       }, 5000);
-
     });
 
   router.resolve();
--- modules/ProductList/ProductList.js
+++ modules/ProductList/ProductList.js
@@ -1,3 +1,4 @@
+import { API_URL } from "../../const";
 import { addContainer } from "../addContainer";
 
 export class ProductList {
@@ -52,24 +53,24 @@ export class ProductList {
     this.containerElement.append(listElem);
   }
 
-  getHTMLTemplateListItem(item) {
+  getHTMLTemplateListItem({id, images: [image], name: title, price}) {
     return `
     <article class="goods__card card">
-    <a class="card__link card__link_img" href="/product/123">
-      <img class="card__img" src="/img/photo.jpg" alt="Кресло с подлокотниками">
+    <a class="card__link card__link_img" href="/product/${id}">
+      <img class="card__img" src="${API_URL}${image}" alt="${title}">
     </a>
 
     <div class="card__info">
       <h3 class="card__title">
-        <a class="card__link" href="/product/123">
-          Кресло с подлокотниками
+        <a class="card__link" href="/product/${id}">
+        ${title}
         </a>
       </h3>
-      <p class="card__price">${item}&nbsp;000&nbsp;₽</p>
+      <p class="card__price">${price.toLocaleString()}&nbsp;₽</p>
     </div>
 
-    <button class="card__btn">В корзину</button>
-    <button class="card__favorite">
+    <button class="card__btn" data-id="${id}">В корзину</button>
+    <button class="card__favorite" data-id="${id}">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
         <path
           d="M8.41325 13.8733C8.18658 13.9533 7.81325 13.9533 7.58659 13.8733C5.65325 13.2133 1.33325 10.46 1.33325 5.79332C1.33325 3.73332 2.99325 2.06665 5.03992 2.06665C6.25325 2.06665 7.32658 2.65332 7.99992 3.55998C8.67325 2.65332 9.75325 2.06665 10.9599 2.06665C13.0066 2.06665 14.6666 3.73332 14.6666 5.79332C14.6666 10.46 10.3466 13.2133 8.41325 13.8733Z"
